706ebfc252e96e6e64fb97dce1910b9fc5c4dfe3
Tiago Alves <tchaguwu@gmail.com>
2022-07-19 16:02:57 -0300

    commit

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+// var c = 1;
+// var galleryPhoto = document.getElementById(`h-gallery-item-${c}`);
+
+// function galleryFlow() {
+//     while(c <= 5){
+
+//         setTimeout( () => {
+//             galleryPhoto.classList.remove("active");
+
+//             c++;
+
+//             galleryPhoto = document.getElementById(`h-gallery-item-${c}`);
+
+//             galleryPhoto.classList.add("active");
+
+//         },
+//         5000);
+
+
+//         if(c = 5){
+//             c = 1;
+//         }
+//     }
+// }
+
+// function galleryClick() {}
